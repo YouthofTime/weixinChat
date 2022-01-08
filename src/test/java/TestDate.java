f348fc32a469b8e63619bacf653110125e8bc9b5
YouthofTime <2698805071@qq.com>
2022-01-08 16:05:30 +0800

    1

--- src/test/java/TestDate.java
+++ src/test/java/TestDate.java
@@ -8,9 +8,10 @@
 //    @Test
 //    public void test(){
 //        Calendar c=Calendar.getInstance();
-//        Date date= DateFormat.parse("2020/12/201",DateFormat.sdfDate);
+//        Date date= DateFormat.parse("2020/12/20",DateFormat.sdfDate);
 //        c.setTime(date);
 //        int i=c.get(Calendar.DAY_OF_WEEK)-1;
 //        System.out.println(i);
 //    }
 //}
+//随便改改 2021/1/8
